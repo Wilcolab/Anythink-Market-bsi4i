e3684d2386bd47dac88b52df23ae4cfef3f2040e
AgathaBahati <58204713+MoigeMatino@users.noreply.github.com>
2022-11-29 19:52:29 +0000

    Fix placeholder image display when no image is uploaded

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -4,6 +4,8 @@ import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 
+const default_image = '/placeholder.png'
+
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
     dispatch({
@@ -36,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image || default_image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
